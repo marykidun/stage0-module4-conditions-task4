d17c2a46fe3421d02f544d2bae2347068d6b6900
Maryna Kidun <marykidun@gmail.com>
2022-10-27 00:08:02 +0300

    Still need to finish

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -1,6 +1,12 @@
 package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
-    public void printGreatest(int first, int second) {
+    public static void printGreatest(int first, int second) {
+        int greatest;
+        greatest = (first > second) ? first : second;
+        System.out.println(greatest);
+    }
+    public static void main(String[] args) {
+        printGreatest(2,3);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -1,6 +1,18 @@
 package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
-    public void printGreatest(int first, int second, int third) {
+    public static void printGreatest(int first, int second, int third) {
+        int max;
+        max = (first > second) ?
+                (first > third ? first : third) :
+                (second > third ? second : third);
+        System.out.println(max);
+
+
+}
+    public static void main(String[] args) {
+        printGreatest(3,4,5);
     }
 }
+
+
