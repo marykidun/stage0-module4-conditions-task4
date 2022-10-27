b688fddc34eca8cad7f7ae97f67f544610a7026b
Maryna Kidun <marykidun@gmail.com>
2022-10-27 23:04:48 +0300

    Finished all tasks

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -1,6 +1,13 @@
 package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
-    public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+    public static void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        String result;
+        result = (first > second) ? "10" : "-10";
+        System.out.println(result);
+
+    }
+    public static void main (String[] args) {
+        assignAndPrintBasedOnWhichBigger(5, 3);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -1,6 +1,12 @@
 package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
-    public void printWhichIsBigger(int first, int second) {
+    public static void printWhichIsBigger(int first, int second) {
+        int bigger;
+        System.out.println(bigger = (first > second) ? first : second);
+    }
+
+    public static void main(String[] args) {
+        printWhichIsBigger(4, 5);
     }
 }
